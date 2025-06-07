d08629993b0979633625009bca84d44256de2fe9
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-06-06 23:11:53 -0700

    improve padding

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { HighlightedArea } from './components/highlighted-area'
 const App: Component = () => {
     return (
         <div class="h-full bg-white">
-            <div class="max-w-4xl mx-auto px-6 py-12 h-full">
+            <div class="max-w-4xl mx-auto pt-6 h-full">
                 <HighlightedArea />
             </div>
         </div>
